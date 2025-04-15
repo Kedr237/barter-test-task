436226e7bb07553216a42f60edbbdc0b007ff3fd
Kedr237 <ll.vanya237@gmail.com>
2025-04-15 13:40:09 +0300

    Setup database

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -51,9 +51,13 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", 5432),
     }
 }
 
