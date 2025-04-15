671f71b0429ef91896071107766f51cfd78ed132
Kedr237 <ll.vanya237@gmail.com>
2025-04-15 20:00:29 +0300

    Setup admin

--- src/ads/admin.py
+++ src/ads/admin.py
@@ -1,18 +1,34 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Ad, Category, ExchangeProposal
 
 
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1
+    fields = ('title',)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'id',)
+    readonly_fields = ('created_at', 'updated_at',)
 
 
 @admin.register(Ad)
 class AdAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'user', 'category', 'condition', 'id',)
+    readonly_fields = ('created_at', 'updated_at',)
+    inlines = (CategoryInline,)
 
 
 @admin.register(ExchangeProposal)
 class ExchangeProposalAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('ad_sender', 'ad_receiver', 'short_comment', 'status', 'id',)
+    readonly_fields = ('created_at', 'updated_at',)
+    
+    def short_comment(self, obj: ExchangeProposal):
+        comment = obj.comment
+        return comment[:20] + '...' if len(comment) > 20 else comment
+    short_comment.short_description = _('Comment')
