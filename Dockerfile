2f4707a254513d04eed215ba91129d34363902c8
Kedr237 <ll.vanya237@gmail.com>
2025-04-15 11:55:18 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.13.2
+
+RUN apt-get update \
+    && apt-get install -y netcat-traditional
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --upgrade pip --no-cache-dir \
+    && pip install -r requirements.txt --no-cache-dir
+
+COPY . .
+
+ENTRYPOINT ["sh", "entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+until nc -z $DB_HOST $DB_PORT; do
+    sleep 1
+done
+
+cd src
+
+python manage.py migrate
+
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.getenv('ADMIN_USERNAME', 'admin')
+email = os.getenv('ADMIN_EMAIL', 'admin@admin.com')
+password = os.getenv('ADMIN_PASSWORD', 'admin')
+
+exists_by_username = User.objects.filter(username=username).exists()
+exists_by_email = User.objects.filter(email=email).exists()
+
+if not (exists_by_username or exists_by_email):
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+    )
+else:
+    print(f'Superuser with "{username}" or "{email}" already exists')
+EOF
+
+gunicorn settings.wsgi:application --bind 0.0.0.0:8000
