571e012f9aa07e78807a417b7e56eb3643101d6e
Kedr237 <ll.vanya237@gmail.com>
2025-04-17 11:48:11 +0300

    Create ad detail page

--- src/ads/urls.py
+++ src/ads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AdListView
+from .views import AdListView, AdDetail
 
 urlpatterns = [
-    path('', AdListView.as_view()),
+    path('', AdListView.as_view(), name='ad_list'),
+    path('ads/<int:id>/', AdDetail.as_view(), name='ad_detail'),
 ]
--- src/ads/views.py
+++ src/ads/views.py
@@ -1,11 +1,18 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Ad
 
 
 class AdListView(ListView):
-    paginate_by = 20
     model = Ad
     template_name = 'ads/ad_list.html'
     context_object_name = 'ads'
     ordering = ['-created_at']
+    paginate_by = 20
+
+
+class AdDetail(DetailView):
+    model = Ad
+    template_name = 'ads/ad_detail.html'
+    context_object_name = 'ad'
+    pk_url_kwarg = 'id'
